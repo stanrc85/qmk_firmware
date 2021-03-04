e2a07aa513abbe223b34c1a4c5f0bb534597446c
stanrc85 <stanrc@gmail.com>
2021-03-04 08:24:47 -0500

    keymap tweaks

--- keyboards/primekb/meridian/keymaps/stanrc85/keymap.c
+++ keyboards/primekb/meridian/keymaps/stanrc85/keymap.c
@@ -26,17 +26,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI,                   KC_LALT, LT_SPCF, LT_SPCF,  TD_TWIN,                                     CA_QUOT, CA_SCLN, KC_MPLY
     ),
     [_DEFAULT] = LAYOUT_split_bs_rshift(
-		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,
-		KC_CTLE, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
+		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          MO(_FN2_60),
 		KC_LCTL, KC_LGUI,                   KC_LALT, KC_SPC,  KC_SPC,  KC_RALT,                                     MO(_FN1_60), KC_RGUI, KC_RCTL
   ),
   	[_FN1_60] = LAYOUT_split_bs_rshift(
-    	KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,
-    	_______, _______, KC_PGUP, _______, _______, _______, _______, _______, KC_UP,   _______, KC_MPRV, KC_MPLY, KC_MNXT,          _______,
-    	KC_CAPS, KC_HOME, KC_PGDN, KC_END,  _______, KC_VOLD, KC_VOLU, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                   _______,
-    	_______, KC_RDP,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PSCR, _______,
+    	KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,
+    	_______, _______, KC_PGUP, KC_VOLU, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, _______, _______, _______,          KC_INS,
+    	KC_CAPS, _______, KC_PGDN, KC_VOLD, _______, _______, _______, KC_LEFT, KC_DOWN,  KC_RGHT, _______, _______,                   _______,
+    	_______,  KC_RDP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PSCR, _______,
     	_______, _______, _______,          _______,          _______,          _______,                   _______, _______, _______
   ),
 	[_FN2_60] = LAYOUT_split_bs_rshift(
